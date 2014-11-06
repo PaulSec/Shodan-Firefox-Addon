46ab8e3bd190f7cc2ade4014e8db5d3429ffaeae
Paul AMAR <aos.paul@gmail.com>
2014-11-06 22:09:00 +0000

    Added basic integration with Shodan

--- data/panel.js
+++ data/panel.js
@@ -1,4 +1,8 @@
-self.port.on("ActiveUrl", function(activeUrlPayload) {
-    var activeTabUrl = document.getElementById("active-tab-url");
-    activeTabUrl.innerHTML = activeUrlPayload;
+self.port.on("ActiveUrl", function(response) {
+    document.getElementById("ip").innerHTML = 'IP: ' + response['ip_str'];
+    document.getElementById("country-name").innerHTML = 'Country: ' + response['country_name'];
+    document.getElementById("city").innerHTML = 'City: ' + response['city'];
+    document.getElementById("ports").innerHTML = 'Ports: ' + response['ports'].toString();
+    // document.getElementById("direct-link").setAttribute('href', 'https://www.shodan.io/host/' + response['ip_str']);
+    // this does not work yet
 });
