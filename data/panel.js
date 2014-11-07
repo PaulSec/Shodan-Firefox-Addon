ede565c25cc66d5859d28e560972fcfb8516e59e
Romain Létendart <contact@romainletendart.fr>
2014-11-07 09:39:03 +0100

    Implemented basic host information retrieval
    Active tab URL is parsed to retrieve host name and requests are sent to Shodan
    to retrieve host information.

--- data/panel.js
+++ data/panel.js
@@ -1,4 +1,19 @@
-self.port.on("ActiveUrl", function(activeUrlPayload) {
-    var activeTabUrl = document.getElementById("active-tab-url");
-    activeTabUrl.innerHTML = activeUrlPayload;
+function displayErrorMessage(errorMessage) {
+    var errorContainer = document.getElementById("error");
+
+    if (errorMessage !== "") {
+        errorContainer.innerHTML = "<p>Error: " + errorMessage + "</p>";
+    } else {
+        errorContainer.innerHTML = "";
+    }
+}
+
+self.port.on("ShodanResponse", function(shodanResponsePayload) {
+    var shodanResponseContainer = document.getElementById("shodan-response");
+    shodanResponseContainer.innerHTML = shodanResponsePayload;
+
+    // Clear any error message
+    displayErrorMessage("");
 });
+
+self.port.on("Error", displayErrorMessage);
