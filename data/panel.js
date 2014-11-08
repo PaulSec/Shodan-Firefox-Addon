ccc1146ab15d35e74ebeefe4413bb9740642d630
Romain Létendart <contact@romainletendart.fr>
2014-11-08 14:47:20 +0000

    Enhanced look & feel of the UI
    Added some CSS to enhance panel appearance.
    Separate host information from information messages in HTML.
    Only display most relevant information to the user.

--- data/panel.js
+++ data/panel.js
@@ -1,19 +1,53 @@
-function displayErrorMessage(errorMessage) {
-    var errorContainer = document.getElementById("error");
-
-    if (errorMessage !== "") {
-        errorContainer.innerHTML = "<p>Error: " + errorMessage + "</p>";
-    } else {
-        errorContainer.innerHTML = "";
-    }
+window.onload = function() {
+    document.getElementById("host").style.display = "none";
+    document.getElementById("info").style.display = "none";
+    document.getElementById("error").style.display = "none";
 }
 
 self.port.on("ShodanResponse", function(shodanResponsePayload) {
-    var shodanResponseContainer = document.getElementById("shodan-response");
-    shodanResponseContainer.innerHTML = shodanResponsePayload;
+    var hostInfo = JSON.parse(shodanResponsePayload);
 
-    // Clear any error message
-    displayErrorMessage("");
+    document.getElementById("ip").innerHTML = hostInfo["ip_str"];
+    document.getElementById("hostname").innerHTML = hostInfo["hostnames"][0];
+
+    var infoToDisplay = {
+        "city": "City",
+        "country_name": "Country",
+        "org": "Organization"
+    };
+    var tableInfo = "";
+    for (var key in infoToDisplay) {
+        if (hostInfo[key] !== null) {
+            tableInfo += "<tr><th>" + infoToDisplay[key] + "</th>";
+            tableInfo += "<td>" + hostInfo[key] + "</td></tr>";
+        }
+    }
+
+    document.getElementById("general-info").innerHTML = tableInfo;
+
+    var portsInfo = "";
+    var portsNumber = hostInfo["ports"].length;
+    for (var i = 0; i <  portsNumber; i++) {
+        portsInfo += "<li><a href=\"#" + hostInfo["ports"][i] + "\">" + hostInfo["ports"][i] + "</a></li>";
+    }
+
+    document.getElementById("ports").innerHTML = portsInfo;
+
+    document.getElementById("host").style.display = "block";
+    document.getElementById("info").style.display = "none";
+    document.getElementById("error").style.display = "none";
 });
 
-self.port.on("Error", displayErrorMessage);
+self.port.on("Error", function(errorMessagePayload) {
+    document.getElementById("error-content").innerHTML = "<p>Error: " + errorMessagePayload + "</p>";
+    document.getElementById("host").style.display = "none";
+    document.getElementById("info").style.display = "none";
+    document.getElementById("error").style.display = "block";
+});
+
+self.port.on("Info", function(infoMessagePayload) {
+    document.getElementById("info-content").innerHTML = "<p>" + infoMessagePayload + "</p>";
+    document.getElementById("host").style.display = "none";
+    document.getElementById("info").style.display = "block";
+    document.getElementById("error").style.display = "none";
+});
