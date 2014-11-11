e97f313240b52e2afb144ee171756352967c0f59
Romain Létendart <contact@romainletendart.fr>
2014-11-11 09:45:29 +0100

    Resize is now almost working

--- data/panel.js
+++ data/panel.js
@@ -33,28 +33,41 @@ self.port.on("ShodanResponse", function(shodanResponsePayload) {
 
     document.getElementById("ports").innerHTML = portsInfo;
 
+    // TODO Put the following code into a function as we have similar code in
+    //      other "self.port.on()".
     document.getElementById("host").style.display = "block";
     document.getElementById("info").style.display = "none";
     document.getElementById("error").style.display = "none";
+    self.port.emit("resize", {
+        width: document.body.clientWidth,
+        height: document.body.clientHeight
+    });
 });
 
 self.port.on("Error", function(errorMessagePayload) {
     document.getElementById("error-content").innerHTML = "<p>Error: " + errorMessagePayload + "</p>";
+
+    // TODO Put the following code into a function as we have similar code in
+    //      other "self.port.on()".
     document.getElementById("host").style.display = "none";
     document.getElementById("info").style.display = "none";
     document.getElementById("error").style.display = "block";
+    self.port.emit("resize", {
+        width: document.body.clientWidth,
+        height: document.body.clientHeight
+    });
 });
 
 self.port.on("Info", function(infoMessagePayload) {
     document.getElementById("info-content").innerHTML = "<p>" + infoMessagePayload + "</p>";
+
+    // TODO Put the following code into a function as we have similar code in
+    //      other "self.port.on()".
     document.getElementById("host").style.display = "none";
     document.getElementById("info").style.display = "block";
     document.getElementById("error").style.display = "none";
-});
-
-self.port.on("resize", function({width, height}) {
-    console.log('test');
-    console.log(width);
-    console.log(height);
-    self.resize(width, height);
+    self.port.emit("resize", {
+        width: document.body.clientWidth,
+        height: document.body.clientHeight
+    });
 });
