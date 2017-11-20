a5e8d009f201e12a4d6bd37b2d3c72ec91c4810c
Paul A <aos.paul@gmail.com>
2017-11-20 21:55:09 +0100

    added new files to be compliant with Firefox

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,10 @@
+function insertAfter(target, el) {
+    if( !target.nextSibling )
+        target.parentNode.appendChild( el );
+    else
+        target.parentNode.insertBefore( el, target.nextSibling );
+};
+
 window.onload = function () {
     var bg = chrome.extension.getBackgroundPage();
     var host = bg.getHost();
@@ -6,51 +13,48 @@ window.onload = function () {
         console.log(host);
         var el = document.getElementById('ip');
         if (el) {
-            el.innerHTML = host.ip_str;
+            el.textContent = host.ip_str;
         }
 
         // Hostnames
-        var hostnames = '';
+        el = document.getElementById('hostnames');
         if (host.hostnames) {
             for (var i = 0; i < host.hostnames.length; i++) {
-                hostnames += '<a href="http://' + host.hostnames[i] + '" target="_blank">' + host.hostnames[i] + '</a>';
+                new_link = document.createElement('a');
+                new_link.setAttribute('href', 'https://' + host.hostnames[i]);
+                new_link.textContent = host.hostnames[i];
+                insertAfter(el, new_link);
             }
         }
-        el = document.getElementById('hostnames');
-        el.innerHTML = hostnames;
 
         // Miscellaneous items (location, operating system etc.)
+        el = document.getElementById('items');
         var items = '';
         if (host.city) {
-            items += '\
-              <tr>\
-                <td>City</td>\
-                <th>' + host.city + '</th>\
-              </tr>';
+            new_row = document.createElement('tr');
+            new_row.insertCell(0).textContent = 'City';
+            new_row.insertCell(1).textContent = host.city;
+            insertAfter(el, new_row);
         }
         if (host.country_name) {
-            items += '\
-              <tr>\
-                <td>Country</td>\
-                <th>' + host.country_name + '</th>\
-              </tr>';
+            new_row = document.createElement('tr');
+            new_row.insertCell(0).textContent = 'Country';
+            new_row.insertCell(1).textContent = host.country_name;
+            insertAfter(el, new_row);
         }
         if (host.os) {
-            items += '\
-              <tr>\
-                <td>Operating System</td>\
-                <th>' + host.os + '</th>\
-              </tr>';
+            new_row = document.createElement('tr');
+            new_row.insertCell(0).textContent = 'Operating System';
+            new_row.insertCell(1).textContent = host.os;
+            insertAfter(el, new_row);
         }
         if (host.org) {
-            items += '\
-              <tr>\
-                <td>Organization</td>\
-                <th style="line-height: 18px">' + host.org + '</th>\
-              </tr>';
+            new_row = document.createElement('tr');
+            new_row.insertCell(0).textContent = 'Organization';
+            new_row.insertCell(1).textContent = host.org;
+            insertAfter(el, new_row);
         }
         el = document.getElementById('items');
-        el.innerHTML = items;
 
         // Ports
         if (host.ports) {
@@ -61,46 +65,79 @@ window.onload = function () {
                 host.ports[i] = parseInt(host.ports[i]);
             }
 
+            el = document.getElementById('ports');
             host.ports.sort(function(a, b){return a-b});
             for (var i = 0; i < host.ports.length; i++) {
                 if (host.ports[i] === 80 || host.ports[i] === 8080 || host.ports[i] === 81) {
                     ports += '<li><a href="http://' + host.ip_str + ':' + host.ports[i] + '" target="_blank"><span>' + host.ports[i] + '</span></a></li>';
+                    // // new span
+                    // new_span = document.createElement('span');
+                    // new_span.textContent = host.ports[i]
+
+                    // // new link
+                    // new_link = document.createElement('a');
+                    // new_link.setAttribute('href', 'http://' + host.ip_str + ':' + host.ports[i]);
+                    // new_link.appendChild(new_span);
+
+                    // // new li
+                    // new_li = document.createElement('li');
+                    // new_li.appendChild(new_link)
+                    // insertAfter(el, new_li);
                 }
                 else if (host.ports[i] === 443 || host.ports[i] === 8443) {
                     ports += '<li><a href="https://' + host.ip_str + ':' + host.ports[i] + '" target="_blank"><span>' + host.ports[i] + '</span></a></li>';
+                    // // new span
+                    // new_span = document.createElement('span');
+                    // new_span.textContent = host.ports[i]
+
+                    // // new link
+                    // new_link = document.createElement('a');
+                    // new_link.setAttribute('href', 'https://' + host.ip_str + ':' + host.ports[i]);
+                    // new_link.appendChild(new_span);
+
+                    // // new li
+                    // new_li = document.createElement('li');
+                    // new_li.appendChild(new_link)
+                    // insertAfter(el, new_li);
                 }
                 else {
-                    ports += '<li><span>' + host.ports[i] + '</span></li>';
+                    ports += '<li><span>' + host.ports[i] + '</span></li>';                    
+                    // // new span
+                    // new_span = document.createElement('span');
+                    // new_span.textContent = host.ports[i]
+
+                    // // new li
+                    // new_li = document.createElement('li');
+                    // new_li.appendChild(new_span)
+                    // insertAfter(el, new_li);
                 }
             }
-
-            el = document.getElementById('ports');
             el.innerHTML = ports;
         }
 
         // Vulnerability information if available
-        var vulns = '';
-        if (host.vulns) {
-            vulns = "<br/>\
-            <h2><i class='icon-fire'></i>Security Issues</h2>\
-            <ul>";
+        // var vulns = '';
+        // if (host.vulns) {
+        //     vulns = "<br/>\
+        //     <h2><i class='icon-fire'></i>Security Issues</h2>\
+        //     <ul>";
 
-            var has_vulns = false;
-            for (var i = 0; i < host.vulns.length; i++) {
-                if (host.vulns[i] === 'CVE-2014-0160') {
-                    vulns += "<li><a href='http://heartbleed.org' target='_blank'><img src='/img/vulns/heartbleed.png' /> Heartbleed (CVE-2014-0160)</a></li>";
-                    has_vulns = true;
-                }
-            }
+        //     var has_vulns = false;
+        //     for (var i = 0; i < host.vulns.length; i++) {
+        //         if (host.vulns[i] === 'CVE-2014-0160') {
+        //             vulns += "<li><a href='http://heartbleed.org' target='_blank'><img src='/img/vulns/heartbleed.png' /> Heartbleed (CVE-2014-0160)</a></li>";
+        //             has_vulns = true;
+        //         }
+        //     }
 
-            vulns += '</ul>';
+        //     vulns += '</ul>';
 
-            if (!has_vulns) {
-                vulns = '';
-            }
-        }
-        el = document.getElementById('vulns');
-        el.innerHTML = vulns;
+        //     if (!has_vulns) {
+        //         vulns = '';
+        //     }
+        // }
+        // el = document.getElementById('vulns');
+        // el.textContent = vulns;
 
         // Update the link to the host details page
         el = document.getElementById('host-link');
